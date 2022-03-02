ddf4e016326c114b21d005e1803bda07f31a0938
trevin-livele <liveletrevin6@gmail.com>
2022-03-02 17:27:26 +0300

    created registration form inputs with form-control

---
+++ accounts/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Account
+
+
+
+class RegistrationForm (forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'confirm Password'
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['first_name','last_name','phone_number','email','password']
+
+
+    
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+
